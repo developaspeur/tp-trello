29cdcc54a07a86cb5e339fc37b9949155d9f6cb1
Jonathan <contact@jonathan-fortune.fr>
2017-02-09 16:34:07 +0100

    Classes Task et User

---
+++ public/js/Task.js
@@ -0,0 +1,89 @@
+/**
+* Classe Task
+* id        : Integer
+* title     : String
+* content   : String
+* DateTask  : Date()
+* _files    : [File]
+* _users    : [User]
+*/
+
+class Task{
+	constructor(){
+		this = {
+			id: null,
+			title: null,
+			content: null,
+      DateTask: new Date(),
+      _files: null,
+      _users:null
+    };
+  }
+
+  // Getters and setters
+	getId(){
+		return this.id;
+	}
+	setId(id){
+		this.id = id;
+		return this;
+	}
+
+	getTitle(){
+		return this.title;
+	}
+	setTitle(title){
+		this.title = title;
+		return this;
+	}
+
+	getContent(){
+		return this.content;
+	}
+	setContent(content){
+		this.content = content;
+		return this;
+	}
+
+  getDate(){
+    return this.DateTask;
+  }
+  setDate(DateTask){
+    this.DateTask = DateTask;
+    return this;
+  }
+
+  // Files
+	getFiles(){
+		return this._files;
+	}
+	addFile(File){
+		if (this._files === null)
+      this._files = [];
+    this._files.push(File.getId());
+    this._files[File.getId()] = File;
+		return this;
+	}
+  removeFile(File){
+    if (this._files !== null)
+      this._files.splice(File.getId(), 1);
+    return this;
+  }
+
+  // Users
+  getUsers(){
+    return this._users;
+  }
+  addUser(User){
+    if (this._users === null)
+      this._users = [];
+    this._users.push(User.getId());
+    this._users[User.getId()] = User;
+    return this;
+  }
+  removeUser(User){
+    if (this._users !== null)
+      this._users.splice(User.getId(), 1);
+    return this;
+  }
+}
---
+++ public/js/User.js
@@ -0,0 +1,61 @@
+/**
+* Classe Task
+* id        : Integer
+* name     	: String
+* password  : String
+* _tasks    : [Task]
+*/
+
+class User{
+	constructor(id){
+		this = {
+			id: id || null,
+			name: null,
+			password: null,
+			_tasks: null
+		};
+	}
+
+	// Getters and setters
+	getId(){
+		return this.id;
+	}
+	setId(id){
+		this.id = id;
+		return this;
+	}
+
+	getName(){
+		return this.name;
+	}
+	setName(name){
+		this.name = name;
+		return this;
+	}
+
+	getPassword(){
+		return this.password;
+	}
+	setPassword(password){
+		this.password = password;
+		return this;
+	}
+
+	// Tasks
+	getTasks(){
+		return this._tasks;
+	}
+	addTask(Task){
+		if (this._tasks === null)
+			this._tasks = [];
+		this._tasks.push(Task.getId());
+		this._tasks[Task.getId()] = Task;
+		return this;
+	}
+	removeFile(Task){
+    if (this._tasks !== null)
+      this._tasks.splice(Task.getId(), 1);
+    return this;
+  }
+
+}
