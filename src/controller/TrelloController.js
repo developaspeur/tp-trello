07a82d7b8264135804ffa39d9c33feeb7729b6fb
Dim <dsurf76@gmail.com>
2017-02-10 09:38:03 +0100

    Commit modif

--- src/controller/TrelloController.js
+++ src/controller/TrelloController.js
@@ -1,3 +1,22 @@
 /**
  * Created by Dimsurf on 09/02/2017.
  */
+var UserDAO = require('../model/UserDAO');
+var UserDTO = require('../model/UserDTO');
+function TrelloController(){}
+
+
+TrelloController.prototype.getAllAction= function(req,res){
+    var name =  req.body.name;
+    var password =  req.body.password;
+    var confirPassword =  req.body.confirmPassword;
+    console.log(name,password,confirPassword);
+
+     res.send('respond with a resource'+ name + password + confirPassword);
+
+};
+
+
+
+
+module.exports = new TrelloController();
--- src/router/dispatcher.js
+++ src/router/dispatcher.js
@@ -1,3 +1,12 @@
 /**
  * Created by Dimsurf on 09/02/2017.
  */
+
+// dispatcher.js
+const express = require('express');
+var dispatcher = express.Router();
+var TrelloRouter = require('./TrelloRouter');
+
+dispatcher.use('/',TrelloRouter);
+
+module.exports = dispatcher;
